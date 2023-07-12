e98d1527dc0a4be1cecb49738ea49df93ac2ff17
Lunkle <d.ren.donny@gmail.com>
2023-07-12 17:28:28 -0400

    Add ProcessorException Javadocs
    Typo

--- src/main/java/derealizer/processor/error/ProcessorException.java
+++ src/main/java/derealizer/processor/error/ProcessorException.java
@@ -1,10 +1,24 @@
 package derealizer.processor.error;
 
+import derealizer.processor.AbstractSingleProcessor;
+import derealizer.processor.DerealizableProcessor;
+
+import javax.annotation.processing.RoundEnvironment;
+import java.util.Set;
+
 import static java.lang.String.format;
 
-public class ProcessorException extends RuntimeException{
+/**
+ * {@link RuntimeException} thrown by the {@link DerealizableProcessor} when an error occurs during processing. Do not
+ * use this exception anywhere else other than in subclasses of {@link AbstractSingleProcessor}.
+ * <br><br>
+ * Internally, this is handled within the {@link AbstractSingleProcessor#process(Set, RoundEnvironment) process} method.
+ *
+ * @author Lunkle
+ */
+public class ProcessorException extends RuntimeException {
 
-    public ProcessorException(String message,  Object... args){
+    public ProcessorException(String message, Object... args) {
         super(format(message, args));
     }
 
